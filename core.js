0a7766e3b8290360d983b7951df39c77d086faf8
BSOD-qian <63780982+BSOD-qian@users.noreply.github.com>
2021-01-23 18:41:29 +0800

    update

---
+++ core.js
@@ -0,0 +1,43 @@
+function search() {
+    if (document.getElementById("input").value != 92011) {
+        if (document.getElementById("input").value != 92022) {
+            if (document.getElementById("input").value != 92033) {
+                if (document.getElementById("input").value != 93442) {
+                    if (document.getElementById("input").value != 93453) {
+                        if (document.getElementById("input").value != 94102) {
+                            if (document.getElementById("input").value != 94113) {
+                                if (document.getElementById("input").value != 94121) {
+                                    if (document.getElementById("input").value != 93121) {
+                                        if (document.getElementById("input").value != 93262) {
+
+                                        } else {
+                                            document.getElementById("output").innerHTML = "1号线<br/>01002<br/>老老八<br/>含特殊扩编车厢、曾为备件车，最后一列扩编";
+                                        }
+                                    } else {
+                                        document.getElementById("output").innerHTML = "1号线<br/>01002<br/>老老八<br/>含特殊扩编车厢、曾为备件车，最后一列扩编";
+                                    }
+                                } else {
+                                    document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+                                }
+                            } else {
+                                document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+                            }
+                        } else {
+                            document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+                        }
+                    } else {
+                        document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+                    }
+                } else {
+                    document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+                }
+            } else {
+                document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+            }
+        } else {
+            document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+        }
+    } else {
+        document.getElementById("output").innerHTML = "1号线<br/>01001<br/>老老八<br/>张江实训专用车";
+    }
+}
